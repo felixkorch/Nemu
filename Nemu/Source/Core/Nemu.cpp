6b832820d84db66dc3b5ccbd24cc6b8fdca842ec
Felix Korch <korch.felix@gmail.com>
2019-02-18 07:12:27 +0100

    more instructions

--- Nemu/Source/Core/Nemu.cpp
+++ Nemu/Source/Core/Nemu.cpp
@@ -12,26 +12,34 @@ int main()
 	std::uint8_t B_HIGH = B >> 8;
 
 	std::vector<std::uint8_t> program = {
-		0xA9, A_LOW,  // lda TAL1_LO      ; Lägg lägsta byten av första talet i ackumulatorregistret
-		0x18,         // clc              ; Rensa carry-flaggan innan addition
-		0x69, B_LOW,  // adc TAL2_LO      ; Addera ackumulatorvärdet med lägsta byten i andra talet
-		0x8D, 0, 0,   // sta RESULTAT_LO  ; Lagra nya ackumulatorvärdet i lägsta byten av resultatet
-		0xA9, A_HIGH, // lda TAL1_HI      ; Lägg högsta byten av första talet i ackumulatorn
-		0x69, B_HIGH, // adc TAL2_HI      ; Addera, men rensa inte carry-flaggan denna gång
-		0x8D, 1, 0    // sta RESULTAT_HI  ; Lagra ackumulatorn i högsta byten av resultatet
+		0xA9, A_LOW,  // lda TAL1_LO
+		0x18,         // clc
+		0x69, B_LOW,  // adc TAL2_LO
+		0x8D, 0, 0,   // sta RESULTAT_LO
+		0xA9, A_HIGH, // lda TAL1_HI
+		0x69, B_HIGH, // adc TAL2_HI
+		0x8D, 1, 0    // sta RESULTAT_HI
 	};
 
 	std::vector<std::uint8_t> program2 = { // Loop
-		0xA9, 10,
-		0x8D, 0, 0,
-		0x4C, 0, 0
+		0xA2, 10,			//0  LDX
+		0x8A,				//2  TXA
+		0x9D, 0, 0,		    //3  STA, X
+		0xCA,				//6  DEX
+		0xF0, 5,			//7  BEQ
+		0x4C, 0x02, 0x80    //9  JMP
+							//12 END
 	};
 
 	CPU cpu;
-	cpu.LoadProgram(program);
+	cpu.LoadProgram(program, 0x8000); // 0x8000 ROM?
 
 	auto ram = cpu.GetRAM();
 	std::cout << A << " + " << B << " = " << *(std::uint16_t*)ram.data() << std::endl;
+	/*
+	for (unsigned int i = 0; i < 10; i++) {
+		std::cout << +ram[i] << std::endl;
+	} */
 
 	std::cin.get();
 }
